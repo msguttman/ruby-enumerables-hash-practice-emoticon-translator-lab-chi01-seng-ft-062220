480be207f7ae19f18935a350003cec8c6e77c0ff
Matt Guttman <msguttman@gmail.com>
2020-04-29 19:22:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,17 @@
 # require modules here
+require 'yaml'
 
-def load_library
+def load_library(file)
   # code goes here
+  translated_hash = Hash.new
+  emoticons_data = YAML.load_file(file)
+  emoticons_data.map { |k, v|
+  translated_hash[k] = {:english => v[0], :japanese => v[1]}
+  }
+translated_hash
 end
+
+
 
 def get_japanese_emoticon
   # code goes here
